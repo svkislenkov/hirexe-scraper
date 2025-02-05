e721f9e0b51b412fe0194b4e0918badf0a5651c0
svkislenkov <svkislenkov@gmail.com>
2025-02-04 19:23:47 -0500

    changes 2/4

--- selen.py
+++ selen.py
@@ -1,21 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+# from selenium.webdriver.common.by import Keys
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+driver.get("https://www.linkedin.com/jobs/")
+
 
 title = driver.title
 
+print(driver.title)
+
 driver.implicitly_wait(0.5)
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+username_box = driver.find_element(by=By.NAME, value="session_key")
+password_box = driver.find_element(by=By.NAME, value="session_password")
 
-text_box.send_keys("Selenium")
+username_box.send_keys("johnhirexe@gmail.com")
+password_box.send_keys("=P_9aGiP7St5Ce3H*T$+")
+
+# submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+submit_button = driver.find_element(By.CSS_SELECTOR, "button[data-id='sign-in-form__submit-btn']")
 submit_button.click()
 
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
+time.sleep(180)
 
 driver.quit()
